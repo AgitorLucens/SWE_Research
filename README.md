762223e03028a3c2034928f2773b8681ddea2092
AgitorLucens <84297807+AgitorLucens@users.noreply.github.com>
2024-04-02 12:06:15 -0600

    Un endpoint activo GET /record, para correr el backend se necesita ejecutar pgadmin 4 con anterioridad

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # SWE_Research
 install Angular
 - npm install -g @angular/cli
+
 create project
 - ng new "project-name" *
 -- *without quotes
+
 run project
 - ng serve
--- backend/go-server/api.go
+++ backend/go-server/api.go
@@ -3,13 +3,25 @@ package main
 import (
 	"log"
 	"net/http"
+	"encoding/json"
 )
-
 
 type APIServer struct {
 	addr string
 }
 
+//Response
+type Response struct {
+	Status int `json:"status"`
+	Data *Record	`json:"data"`
+
+}
+
+type ResponseData struct {
+	Status int `json:"status"`
+	Data any	`json:"data"`
+
+}
 func NewAPIServer(addr string) *APIServer{
 	return &APIServer{
 		addr: addr,
@@ -18,12 +30,39 @@ func NewAPIServer(addr string) *APIServer{
 
 func (s *APIServer) Run() error {
 	router := http.NewServeMux()
-	router.HandleFunc("POST /users",func(w http.
-		ResponseWriter, r *http.Request){
-			userId :="fiorella hola"
-			w.Write([]byte("User ID: " + userId))
-		})
 
+	DB := newAPIDB("user=postgres password=dontwell12 dbname=swe-research-db host=localhost port=5432 sslmode=disable")
+	err := DB.initDB()
+
+	//end  points
+	router.HandleFunc("GET /record", func(w http.
+		ResponseWriter, r *http.Request){
+
+			data, _ := extractRecord(DB.DB)
+
+			if err != nil {
+				http.Error(w, "Error al obtener registros", http.StatusInternalServerError)
+				log.Fatal(err)
+			}
+
+			
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusCreated)
+			//log.Printf("%s", data.Descr)
+			json.NewEncoder(w).Encode(&Response{ http.StatusOK, &Record{
+				Name: data.Name,
+				Price: data.Price,
+				Descr: data.Descr,
+			}})
+
+			
+			if err != nil {
+            	http.Error(w, "Error al codificar respuesta JSON", http.StatusInternalServerError)
+            return
+        	}
+			
+		})//end endpoint
+	
 	server := http.Server{
 		Addr: s.addr,
 		Handler: router,
